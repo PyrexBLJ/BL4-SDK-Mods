7b9be774e356683c10c5a6163b72005faaf890de
PyrexBLJ <49227284+PyrexBLJ@users.noreply.github.com>
2026-03-08 23:53:28 -0400

    map tp (bonk 1.0.1)

--- BonkUtilities/__init__.py
+++ BonkUtilities/__init__.py
@@ -3,15 +3,25 @@ from mods_base import build_mod, get_pc, keybind, hook, ENGINE, SliderOption, Sp
 from unrealsdk.hooks import Type, Block
 from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct, IGNORE_STRUCT
 from typing import Any
+from threading import Thread
+import time
 
 from .commands import *
 
 ConsoleFontSize: SliderOption = SliderOption("Console Font Size", 18, 1, 128, 1, True, on_change = lambda _, new_value: setConsoleFontSize(_, new_value))
 #NoclipSpeed: SliderOption = SliderOption("Noclip Speed", 12000, 100, 25000, 1, True, description="I for the life of me could not get this to work")
 NoBMViewCooldown: BoolOption = BoolOption("No BM Cooldown Without Purchase", True, "Yes", "No")
+MapTP: BoolOption = BoolOption("Teleport to Custom Map Pins", False, "Yes", "No", description="Quickly make and remove a pin on the map to teleport to that location, does not work if you pin a pre-existing marker on the map.")
+MapTPWindow: SliderOption = SliderOption("Map Teleport Pin Window", 2.0, 0.5, 4.0, 0.1, False, description="How long (in seconds) you have to remove the map pin after making it to teleport to it.")
+
+
 noclip: bool = False
 godmode: bool = False
-
+# map tp stuff
+pintime = 0
+pinlocation = None
+teleported = False
+#
 
 
 def setConsoleFontSize(_: SliderOption, new_value: int) -> None:
@@ -171,8 +181,51 @@ def BlackMarketUncloak(obj: UObject, args: WrappedStruct, ret: Any, func: BoundF
         obj.bCooldownOnView = False
     return None
 
+@hook("/Script/OakGame.OakPlayerState:Server_CreateDiscoveryPin", Type.POST)
+def CreatePin(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> None:
+    global pintime, pinlocation
+    if MapTP.value:
+        if args.InPinData.pintype == 1:
+            pintime = time.time()
+            pinlocation = args.InPinData.PinnedCustomWaypointLocation
+    return None
+
+@hook("/Script/OakGame.OakPlayerState:Server_RemoveDiscoveryPin", Type.POST)
+def RemovePin(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> None:
+    global pintime, pinlocation, teleported
+    if MapTP.value:
+        if time.time() - pintime < MapTPWindow.value:
+            pinlocation.Z = 50000
+            get_pc().OakCharacter.K2_SetActorLocation(pinlocation, False, IGNORE_STRUCT, False)
+            pinlocation.Z = -1000
+            get_pc().OakCharacter.K2_SetActorLocation(pinlocation, True, IGNORE_STRUCT, False)
+            teleported = True
+            notify(f"Teleported to {get_pc().K2_GetActorLocation()}")
+    return None
+
+def threadtp() -> None:
+    global pinlocation, teleported
+    if teleported:
+        teleported = False
+        time.sleep(2)
+        if get_pc().OakCharacter.OakCharacterMovement.MovementMode in (3, 4, 6):
+            get_pc().OakCharacter.OakCharacterMovement.MovementMode = 1
+            pinlocation.Z = 50000
+            get_pc().OakCharacter.K2_SetActorLocation(pinlocation, False, IGNORE_STRUCT, False)
+            pinlocation.Z = -1000
+            get_pc().OakCharacter.K2_SetActorLocation(pinlocation, True, IGNORE_STRUCT, False)
+    return None
+
+@hook("/Game/UI/Scripts/ui_script_menu_base.ui_script_menu_base_C:MenuClose", Type.POST)
+def MenuClose(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> None:
+    global pintime, pinlocation, teleported
+    if MapTP.value:
+        Thread(target=threadtp).start()
+    return None
+
+
 def Enable() -> None:
     unrealsdk.find_object("Font", "/Engine/Transient.DefaultRegularFont").LegacyFontSize = ConsoleFontSize.value
     return None
 
-build_mod(on_enable=Enable, options=[ConsoleFontSize, NoBMViewCooldown])
+build_mod(on_enable=Enable, options=[ConsoleFontSize, NoBMViewCooldown, MapTP, MapTPWindow])
